abda9e48314a5d3f06bbd179943b9f064bb04992
Dip-Ghosh <dipnsucse@gmail.com>
2020-12-19 16:51:00 +0600

    Logout api done

--- we-dev/app/Http/Controllers/AuthController.php
+++ we-dev/app/Http/Controllers/AuthController.php
@@ -56,37 +56,6 @@ class AuthController extends Controller
 
     }
 
-    public function postLogin(Request $request)
-    {
-        $this->validate($request, [
-            'email'    => 'required|email|max:255',
-            'password' => 'required',
-        ]);
-
-        try {
-
-            if (! $token = $this->jwt->attempt($request->only('email', 'password'))) {
-                return response()->json(['user_not_found'], 404);
-            }
-
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-
-            return response()->json(['token_expired'], 500);
-
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-
-            return response()->json(['token_invalid'], 500);
-
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-
-            return response()->json(['token_absent' => $e->getMessage()], 500);
-
-        }
-
-        return response()->json(compact('token'));
-    }
-
-
     /**
      * Get a JWT via given credentials.
      *
@@ -108,5 +77,17 @@ class AuthController extends Controller
         }
 
         return $this->respondWithToken($token);
+    }
+
+    public function logout(Request $request)
+    {
+        $token =  str_replace('Bearer ','',$request->header('Authorization'));
+        $this->jwt->setToken($token)->invalidate();
+        $this->jwt->setToken($token)->invalidate(true);
+        Auth::logout();
+        $this->jwt->invalidate($this->jwt->getToken());
+        $this->jwt->parseToken()->invalidate();
+
+        return ['message'=>'Token removed'] ;
     }
 }
